6218e43d0b80fa1d8858db25e6b545407dd72d9a
Crazy <crazycodeboy@gmail.com>
2021-05-29 08:19:16 +0800

    null safety

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.0.1] - TODO: Add release date.
+## [0.0.3] - support null safety.
+## [0.0.2] - v2
 
-* TODO: Describe initial release.
--- lib/underline_indicator.dart
+++ lib/underline_indicator.dart
@@ -42,35 +42,35 @@ class UnderlineIndicator extends Decoration {
   final EdgeInsetsGeometry insets;
 
   @override
-  Decoration lerpFrom(Decoration a, double t) {
+  Decoration? lerpFrom(Decoration? a, double t) {
     if (a is UnderlineIndicator) {
       return UnderlineIndicator(
         borderSide: BorderSide.lerp(a.borderSide, borderSide, t),
-        insets: EdgeInsetsGeometry.lerp(a.insets, insets, t),
+        insets: EdgeInsetsGeometry.lerp(a.insets, insets, t)!,
       );
     }
     return super.lerpFrom(a, t);
   }
 
   @override
-  Decoration lerpTo(Decoration b, double t) {
+  Decoration? lerpTo(Decoration? b, double t) {
     if (b is UnderlineIndicator) {
       return UnderlineIndicator(
         borderSide: BorderSide.lerp(borderSide, b.borderSide, t),
-        insets: EdgeInsetsGeometry.lerp(insets, b.insets, t),
+        insets: EdgeInsetsGeometry.lerp(insets, b.insets, t)!,
       );
     }
     return super.lerpTo(b, t);
   }
 
   @override
-  _UnderlinePainter createBoxPainter([ VoidCallback onChanged ]) {
+  _UnderlinePainter createBoxPainter([ VoidCallback? onChanged ]) {
     return _UnderlinePainter(this.strokeCap,this, onChanged);
   }
 }
 
 class _UnderlinePainter extends BoxPainter {
-  _UnderlinePainter(this.strokeCap, this.decoration, VoidCallback onChanged)
+  _UnderlinePainter(this.strokeCap, this.decoration, VoidCallback? onChanged)
       : assert(decoration != null),
         super(onChanged);
   ///Styles to use for line endings.
@@ -96,8 +96,8 @@ class _UnderlinePainter extends BoxPainter {
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
     assert(configuration != null);
     assert(configuration.size != null);
-    final Rect rect = offset & configuration.size;
-    final TextDirection textDirection = configuration.textDirection;
+    final Rect rect = offset & configuration.size!;
+    final TextDirection textDirection = configuration.textDirection!;
     final Rect indicator = _indicatorRectFor(rect, textDirection).deflate(borderSide.width / 2.0);
     final Paint paint = borderSide.toPaint()..strokeCap = strokeCap;
     canvas.drawLine(indicator.bottomLeft, indicator.bottomRight, paint);
